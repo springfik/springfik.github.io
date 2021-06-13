fa29fc1e5e41a8cb98fe7cf681e2bdec7913a05f
springfik <85852448+springfik@users.noreply.github.com>
2021-06-14 06:55:49 +0700

    Add files via upload

---
+++ client.js
@@ -0,0 +1,33 @@
+'use strict'
+
+navigator.sayswho= (function(){
+    var ua= navigator.userAgent, tem, 
+    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if(/trident/i.test(M[1])){
+        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE '+(tem[1] || '');
+    }
+    if(M[1]=== 'Chrome'){
+        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+    return M.join(' ');
+})();
+
+
+
+var sender = document.getElementById('sender');
+
+
+
+ sender.onclick = function (e) {
+
+  
+
+  var f_name = document.getElementById('f_name').value;
+  var s_name = document.getElementById('s_name').value;
+
+  alert('Доброго времени суток ' + f_name + ' ' + s_name);
+}; 
